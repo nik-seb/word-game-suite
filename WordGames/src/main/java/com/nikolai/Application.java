7d7ef5c070c5f41a2d0760c416acaa8d48f920fb
Nikolai Humphrey-Blanco <nikolai.humphreyb@gmail.com>
2022-02-11 20:04:17 -0500

    Populate VocabList vocab with source file

--- WordGames/src/main/java/com/nikolai/Application.java
+++ WordGames/src/main/java/com/nikolai/Application.java
@@ -1,8 +1,14 @@
 package com.nikolai;
 
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
-
+        VocabList vocabList = new VocabList();
+        List<String> vocab = vocabList.getVocab();
+        for (String word : vocab) {
+            System.out.println(word);
+        }
     }
 }
--- WordGames/src/main/java/com/nikolai/VocabList.java
+++ WordGames/src/main/java/com/nikolai/VocabList.java
@@ -4,18 +4,35 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class VocabList {
-    List<String> vocab = new ArrayList<>();
+    private List<String> vocab = new ArrayList<>();
 
     public VocabList () {
-        File sourceFile = new File("resources/java-vocab.txt");
+        File sourceFile = new File("src/main/resources/java-vocab.txt");
+        if (!sourceFile.canRead() || !sourceFile.exists() || !sourceFile.isFile()) {
+            System.out.println("File error.");
+        }
         try (Scanner fileScanner = new Scanner(sourceFile)) {
-            
+            while (fileScanner.hasNextLine()) {
+                String currentLine = fileScanner.nextLine();
+                if (currentLine.length() > 0) {
+                    vocab.add(currentLine);
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Error: File not found.");
+        } catch (NoSuchElementException e) {
+            System.out.println("No such element: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    public List<String> getVocab () {
+        return new ArrayList<>(vocab);
     }
 
 }
